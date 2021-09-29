40c6bac8860a5469f521c9905df9df2852d43cba
kevslinger <siouxman@me.com>
2021-09-29 19:46:16 -0400

    Fixed the mobile navbar height issue, added task2animation with shadow having a snapebot above their shoulder

--- src/task2/task2Animation.js
+++ src/task2/task2Animation.js
@@ -1,4 +1,3 @@
-import React from "react";
 import "./task2Animation.css";
 import Task1Animation from "../task1/task1Animation";
 import {Delay} from "../utils";
@@ -7,7 +6,7 @@ class Task2Animation extends Task1Animation {
     constructor(props) {
         super(props);
         this.state.part2Script = [
-            `Hello again, Team ${props.house}! How goes the quest to become the great team of Pottamon masters` +
+            `Hello again, Team ${props.house}! How goes the quest to become the great team of Pottamon masters ` +
                 "the world has ever seen?",
             "And how about the work on learning more about these magical creatures and the elements they harness?",
             "I'm certainly impressed with the reports your reps have sent me, you're advancing our course of study " +
@@ -20,19 +19,18 @@ class Task2Animation extends Task1Animation {
                 "beef noodle soup is up to snuff.",
             "Oh, you want a gym badge? Okay, I guess we can do that too... *after* I get some dumplings.",
             "Prof. Squash: There is a Psychic gym off Route 16 in Zimbardo Village. The gym leader there... well, Ra's " +
-                "will have a most interesting challenge all who knock knock upon his door.",
+                "will have a most interesting challenge for all who knock knock upon his door.",
             "Ra's: Baby, I'm a Mismagius. I'll make all your wildest dreams come true... That is, provided you can get me " +
-                "STOKED with your best jokes or *checks notes* memes, as they call them.",
+                "STOKED with your best jokes!",
             "My gym challenge is one of mental exercises to show me your creativity and wit. They say you can learn a lot " +
                 "about someone by their humor... SHOW ME YOURS!!!",
             "Prof. Squash: And going out west on Route 18 to Ethelene Center will lead you to the mysterious Poison gym. " +
-                "We don't know much about it, or its gym leader, Shadow, but whatever you do, don't stare into the mask.",
+                "We don't know much about it, or its gym leader, ShadowOfApollo, but whatever you do, don't stare into the mask.",
             "Shadow: So, you're going to pay me a visit? That's unfortunate. Being around other people annoys me, " +
                 "especially eager young researchers.",
-            "Oh well, that doesn't matter. Let's see if you can escape my cave. Good luck... Oh, and don't let the snapebats bite.",
+            "Well, that doesn't matter. Let's see if you can escape my cave. Good luck... Oh, and don't let the snapebats bite.",
             `Prof. Squash: Thanks for all your help so far! We've learned a lot about the ${this.props.pottamon} since ` +
-                "you discovered it!"
-            ,
+                "you discovered it!",
             `Well don't just stand there, Team ${props.house}! Go! Run! Earn more badges!`
         ];
         this.state.house = props.house;
@@ -42,7 +40,7 @@ class Task2Animation extends Task1Animation {
         this.state.hideProf = false;
         this.state.leader1Path = "spludgiexx.png";
         this.state.leader2Path = "ra.png";
-        this.state.leader3Path = "shadow.png";
+        this.state.leader3Path = "shadow_with_snapebat.png";
     }
 
     continueGame = async() => {
