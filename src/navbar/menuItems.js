d0dc858951e9ccd74477fd118d2c11dad9c6f2b3
kevslinger <esslinger.k@northeastern.edu>
2021-09-30 23:55:06 -0400

    Remove everything except the intro and homepage

--- src/navbar/menuItems.js
+++ src/navbar/menuItems.js
@@ -9,64 +9,64 @@ export const MENUITEMS = [
         url: '/intro',
         cName: 'nav-links'
     },
-    {
-        title: 'Gyms 1-3 Homepage',
-        url: '/part1',
-        cName: 'nav-links'
-    },
-    {
-        title: 'Rock Gym',
-        url: '/rock',
-        cName: 'nav-links'
-    },
-    {
-        title: 'Water Gym',
-        url: '/water',
-        cName: 'nav-links'
-    },
-    {
-        title: 'Electric Gym',
-        url: '/electric',
-        cName: 'nav-links'
-    },
-    {
-        title: 'Gyms 4-6 Homepage',
-        url: '/part2',
-        cName: 'nav-links'
-    },
-    {
-        title: 'Grass Gym',
-        url: '/grass',
-        cName: 'nav-links'
-    },
-    {
-        title: 'Psychic Gym',
-        url: '/psychic',
-        cName: 'nav-links'
-    },
-    {
-        title: 'Poison Gym',
-        url: '/poison',
-        cName: 'nav-links'
-    },
-    {
-        title: 'Horcrux Cave',
-        url: '/snapebat',
-        cName: 'nav-links'
-    },
-    {
-        title: 'Gyms 7-8 Homepage',
-        url: '/part3',
-        cName: 'nav-links'
-    },
-    {
-        title: 'Fire Gym',
-        url: '/fire',
-        cName: 'nav-links'
-    },
-    {
-        title: 'Ground Gym',
-        url: '/ground',
-        cName: 'nav-links'
-    }
+    // {
+    //     title: 'Gyms 1-3 Homepage',
+    //     url: '/part1',
+    //     cName: 'nav-links'
+    // },
+    // {
+    //     title: 'Rock Gym',
+    //     url: '/rock',
+    //     cName: 'nav-links'
+    // },
+    // {
+    //     title: 'Water Gym',
+    //     url: '/water',
+    //     cName: 'nav-links'
+    // },
+    // {
+    //     title: 'Electric Gym',
+    //     url: '/electric',
+    //     cName: 'nav-links'
+    // },
+    // {
+    //     title: 'Gyms 4-6 Homepage',
+    //     url: '/part2',
+    //     cName: 'nav-links'
+    // },
+    // {
+    //     title: 'Grass Gym',
+    //     url: '/grass',
+    //     cName: 'nav-links'
+    // },
+    // {
+    //     title: 'Psychic Gym',
+    //     url: '/psychic',
+    //     cName: 'nav-links'
+    // },
+    // {
+    //     title: 'Poison Gym',
+    //     url: '/poison',
+    //     cName: 'nav-links'
+    // },
+    // {
+    //     title: 'Horcrux Cave',
+    //     url: '/snapebat',
+    //     cName: 'nav-links'
+    // },
+    // {
+    //     title: 'Gyms 7-8 Homepage',
+    //     url: '/part3',
+    //     cName: 'nav-links'
+    // },
+    // {
+    //     title: 'Fire Gym',
+    //     url: '/fire',
+    //     cName: 'nav-links'
+    // },
+    // {
+    //     title: 'Ground Gym',
+    //     url: '/ground',
+    //     cName: 'nav-links'
+    // }
 ];
