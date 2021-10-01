c1485701fbaf9c513c4f2f60265342a2fa3714dc
kevslinger <siouxman@me.com>
2021-09-30 23:13:47 -0400

    Disabled horcrux cave for safari

--- src/zubat-cave/game.js
+++ src/zubat-cave/game.js
@@ -6,6 +6,11 @@ import { GoogleSpreadsheet } from "google-spreadsheet";
 import { capitalize } from "../utils";
 import {Link} from "react-router-dom";
 
+var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && window['safari'].pushNotification));
+console.log("sfar");
+console.log(isSafari);
+console.log("sfar");
+
 class ZubatMaze extends React.Component {
     constructor(props) {
         super(props);
@@ -135,81 +140,102 @@ class ZubatMaze extends React.Component {
                 <div className={"zubat-game-mobile-error-container"}>
                     <h1>We're Sorry!</h1>
                     <p>The Horcrux Cave game is not supported on mobile. Sorry!<br/><br/>If you are receiving this error
-                        on a computer, please send a reddit PM to u/Professor_Squash so we can address this issue. Thanks!
+                        on a computer, try putting your webpage in full-screen mode. If that does not fix the issue,
+                        please send a reddit PM to u/Professor_Squash so we can address this issue. Thanks!
                     </p>
                 </div>
-                <div className={"zubat-game-container"}>
-                    {!this.state.gameLive ?
-                        <form className={"zubat-game-registration-form"} onSubmit={this.beginGame}>
-                            <h1 className={"zubat-game-welcome"}>Welcome to the Poison Gym Challenge!</h1>
-                            <p className={"zubat-game-welcome-rules"}>Oh no! You're lost in the Horcrux Cave with no escape rope! Navigate to the exit with the arrow keys
-                                or WASD. Try to avoid as many Zubats as you can along the way, good luck!</p>
-                            <label className={"zubat-game-form-label"}>{this.state.formUsernameLabel}</label>
-                            <br/>
-                            <input className={"zubat-game-form-label"} type={"text"} onChange={this.handleUsernameFormChange.bind(this)}/>
-                            <br/>
-                            <label className={"zubat-game-form-label"}>{this.state.formHouseLabel}</label>
-                            <br/>
-                            <input className={"zubat-game-form-label"} type={"text"} onChange={this.handleHouseFormChange.bind(this)}/>
-                            <br/>
-                            <input className={"zubat-game-form-label"} type={"submit"} style={{"width": "25%", "marginTop": "10px"}} value={"Play!"}/>
-                        </form>
-                    : null }
-                    <div style={{"width": "100%", "height": "100%", "position": "relative"}}>
-                        <img style={{"position": "relative",
-                            "top": `${100 - (this.state.cave.y+1) * this.state.percentSize}%`,
-                            "left": `${(this.state.cave.x) * this.state.percentSize}%`,
-                            "height": "10%",
-                            "width": "5%",
-                            "zIndex": 1,
-                            "opacity": this.state.completed ? 0 : 1}} src={this.state.trainerSpritePath} alt={"YOU"}/>
-                        <span className={"exit"}/>
-                        {
-                            this.state.cave.zubats.map((item, idx) => (
-                                <img className={"zubat"} key={idx}
-                                     style={{"left": `${item.x * this.state.percentSize}%`,
-                                             "top": `${100 - (item.y+1) * this.state.percentSize - 3}%`,
-                                             }}
-                                     src={"snapebat.png"}
-                                     alt={"snapebat"}/>
-                            ))
-                        }
-                        <Confetti active={this.state.completed}
-                                  config={
-                                      {
-                                          angle: "270",
-                                          spread: "360",
-                                          startVelocity: "40",
-                                          elementCount: 270,
-                                          duration: 5000,
-                                          height: "5px",
-                                          width: "5px",
+                {isSafari ?
+                    <div className={"zubat-game-safari-error-container"}>
+                        <h1>We're Sorry!</h1>
+                        <p>The Horcrux game does not support Safari. We recommend using Firefox or Google Chrome. If you
+                            are receiving this error message and you're <strong>not</strong> using Safari, please send a
+                            reddit PM to u/Professor_Squash so we can address this issue. Thanks!</p>
+                    </div>
+                    :
+                    <div className={"zubat-game-container"}>
+                        {!this.state.gameLive ?
+                            <form className={"zubat-game-registration-form"} onSubmit={this.beginGame}>
+                                <h1 className={"zubat-game-welcome"}>Welcome to the Poison Gym Challenge!</h1>
+                                <p className={"zubat-game-welcome-rules"}>Oh no! You're lost in the Horcrux Cave with no
+                                    escape rope! Navigate to the exit with the arrow keys
+                                    or WASD. Try to avoid as many Zubats as you can along the way, good luck!</p>
+                                <label className={"zubat-game-form-label"}>{this.state.formUsernameLabel}</label>
+                                <br/>
+                                <input className={"zubat-game-form-label"} type={"text"}
+                                       onChange={this.handleUsernameFormChange.bind(this)}/>
+                                <br/>
+                                <label className={"zubat-game-form-label"}>{this.state.formHouseLabel}</label>
+                                <br/>
+                                <input className={"zubat-game-form-label"} type={"text"}
+                                       onChange={this.handleHouseFormChange.bind(this)}/>
+                                <br/>
+                                <input className={"zubat-game-form-label"} type={"submit"}
+                                       style={{"width": "25%", "marginTop": "10px"}} value={"Play!"}/>
+                            </form>
+                            : null}
+                        <div style={{"width": "100%", "height": "100%", "position": "relative"}}>
+                            <img style={{
+                                "position": "relative",
+                                "top": `${100 - (this.state.cave.y + 1) * this.state.percentSize}%`,
+                                "left": `${(this.state.cave.x) * this.state.percentSize}%`,
+                                "height": "10%",
+                                "width": "5%",
+                                "zIndex": 1,
+                                "opacity": this.state.completed ? 0 : 1
+                            }} src={this.state.trainerSpritePath} alt={"YOU"}/>
+                            <span className={"exit"}/>
+                            {
+                                this.state.cave.zubats.map((item, idx) => (
+                                    <img className={"zubat"} key={idx}
+                                         style={{
+                                             "left": `${item.x * this.state.percentSize}%`,
+                                             "top": `${100 - (item.y + 1) * this.state.percentSize - 3}%`,
+                                         }}
+                                         src={"snapebat.png"}
+                                         alt={"snapebat"}/>
+                                ))
+                            }
+                            <Confetti active={this.state.completed}
+                                      config={
+                                          {
+                                              angle: "270",
+                                              spread: "360",
+                                              startVelocity: "40",
+                                              elementCount: 270,
+                                              duration: 5000,
+                                              height: "5px",
+                                              width: "5px",
+                                          }
                                       }
-                                  }
-                        />
+                            />
 
-                        {this.state.completed ?
-                            <div>
-                            <h1 className={"zubat-endgame-congrats-h1"} >CONGRATS</h1>
-                            <p className={"zubat-endgame-score-p"}>
-                                You ran into {this.state.cave.zubatCount} zubat{this.state.cave.zubatCount !== 1? "s" : null}!</p>
-                            <button className={"zubat-endgame-buttons"} onClick={this.reset.bind(this)}>Play Again</button>
-                            <br/>
-                            <Link to={"/home"}>
-                                <button className={"zubat-endgame-buttons"}>Return to Home</button>
-                            </Link>
-                            </div>
-                        : null}
+                            {this.state.completed ?
+                                <div>
+                                    <h1 className={"zubat-endgame-congrats-h1"}>CONGRATS</h1>
+                                    <p className={"zubat-endgame-score-p"}>
+                                        You ran
+                                        into {this.state.cave.zubatCount} zubat{this.state.cave.zubatCount !== 1 ? "s" : null}!</p>
+                                    <button className={"zubat-endgame-buttons"} onClick={this.reset.bind(this)}>Play
+                                        Again
+                                    </button>
+                                    <br/>
+                                    <Link to={"/home"}>
+                                        <button className={"zubat-endgame-buttons"}>Return to Home</button>
+                                    </Link>
+                                </div>
+                                : null}
+                        </div>
+                        <div className={"zubat-scoreboard-container"}>
+                            <h1 className={"zubat-game-name"}>HORCRUX CAVE ESCAPE</h1>
+                            <h1 className={"zubat-scoreboard"}>SNAPEBATS ENCOUNTERED: {this.state.cave.zubatCount}</h1>
+                        </div>
+                        <div className={"zubat-rules"}>
+                            <h1>Welcome to the Poison Gym Challenge!</h1>
+                            <p>Use the arrow keys or WASD to move. Navigate through the Horcrux Cave and try to avoid as
+                                many zubats as you can!</p>
+                        </div>
                     </div>
-                    <div className={"zubat-scoreboard-container"}>
-                        <h1 className={"zubat-game-name"}>HORCRUX CAVE ESCAPE</h1>
-                        <h1 className={"zubat-scoreboard"}>SNAPEBATS ENCOUNTERED: {this.state.cave.zubatCount}</h1>
-                    </div>
-                    <div className={"zubat-rules"}>
-                        <h1>Welcome to the Poison Gym Challenge!</h1>
-                        <p>Use the arrow keys or WASD to move. Navigate through the Horcrux Cave and try to avoid as many zubats as you can!</p>
-                    </div>
-                </div>
+                }
             </div>
         );
     }
